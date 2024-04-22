7cfec029b9ba75b181fc1bb1796ebc8e942baf5b
marcrebullida2004 <marcrebullidalozano@gmail.com>
2024-04-22 21:04:35 +0200

    Poner un set a loop_start dia 2.
    Todavia no funciona
    
    Hemos hemos el guild de un servidor nuevo al bot, asi que ahora tiene dos guilds (creo que eso da problemas)

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ load_dotenv()
 bot = lightbulb.BotApp(
     token=os.environ["DISCORD_TOKEN"],
     prefix=os.environ["DISCORD_PREFIX"],
-    default_enabled_guilds=1214648265836990524,
+    default_enabled_guilds=[1214648265836990524, 1232023091832557618],
     intents=hikari.Intents.ALL_MESSAGES
     | hikari.Intents.GUILDS
     | hikari.Intents.MESSAGE_CONTENT
